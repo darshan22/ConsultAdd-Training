04a5a1cc682d335a67be4958d16ae16f8c67f9fb
Darshan Kasat <darshankasat22@gwu.edu>
2019-09-17 01:57:23 -0400

    add search_word() and remove_duplicates() function and txt file

--- python_task.py
+++ python_task.py
@@ -1,3 +1,4 @@
+import re
 def is_palindrome(string):
 	if len(string) == 0:
 		return False
@@ -20,5 +21,30 @@ def map_length(input_words):
 	words_list = input_words.split()
 	return [len(word) for word in words_list]
 
+#input_words = input("Please enter the words separated by space:\n")
+#print(map_length(input_words))
+
+def search_word(text_file):
+	file_name = "./" + text_file
+	f = open(file_name, 'r')
+	pattern = 'Darshan'
+	if re.search(pattern, f.read()):
+		print("Match Found")
+	else:
+		print("No match found")
+
+search_word("abc.txt")
+
+def remove_duplicates(input_words_without_space):
+	list_words = input_words_without_space.split()
+	if len(list_words) == 0:
+		return None
+	
+	if len(list_words) == 1:
+		return list_words
+
+	else:
+		return list(set(list_words))
+
 input_words = input("Please enter the words separated by space:\n")
-print(map_length(input_words))
+print(remove_duplicates(input_words))
